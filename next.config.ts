b9b95dafe265df1cd3fe7f997b1d438f5ae68506
DimasGodim <dimas.ngadinegaran@gmail.com>
2026-02-09 01:28:01 +0700

    tambahkan image remote pattern mengijinkan cloudinary dan api pribadi

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,27 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // backend local / VPS kamu
       {
         protocol: "https",
         hostname: "be-sporton.agunacourse.com",
         pathname: "/uploads/**",
       },
+
+      // cloudinary
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+
+      {
+        protocol: "https",
+        hostname: "sporton-be-production-29ef.up.railway.app",
+        pathname: "/data/**",
+      },
     ],
   },
 };
-
 
 export default nextConfig;
